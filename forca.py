919a50608b7d9aaeaabc782783bce99774819b1e
Rodrigo Lima <rodrigo.blima17@gmail.com>
2023-08-10 10:54:55 -0300

    Aplicando for no lugar dos if

--- forca.py
+++ forca.py
@@ -9,7 +9,7 @@ base1 =  "||"
 base2 =  "||"
 
 dica = ""
-l1 = l2 = l3 = l4 = l5 = l6 = l7 = l8 = l9 = "_"
+l = ["_" for _ in range(9)]
 palpites = ""
 erro = acerto = 0
 
@@ -41,7 +41,7 @@ while(acerto < 9 and erro < 6):
     print(pernas)
     print(base1)
     print(base2)
-    print(" ",l1,l2,l3,l4,l5,l6,l7,l8,l9)
+    print(" ",l)
     print(palpites)
 
     print("Digite uma letra")
@@ -53,50 +53,11 @@ while(acerto < 9 and erro < 6):
     
     #testando se acertou ou errou
     if letra in palavra: 
-        if letra == palavra[0]:
-            l1 = letra
-            palavra[0] = "@"
-            acerto += 1
-
-        if letra == palavra[1]:
-            l2 = letra
-            palavra[1] = "@"
-            acerto += 1
-
-        if letra == palavra[2]:
-            l3 = letra
-            palavra[2] = "@"
-            acerto += 1
-            
-        if letra == palavra[3]:
-            l4 = letra
-            palavra[3] = "@"
-            acerto += 1
-
-        if letra == palavra[4]:
-            l5 = letra
-            palavra[4] = "@"
-            acerto += 1
-
-        if letra == palavra[5]:
-            l6 = letra
-            palavra[5] = "@"
-            acerto += 1
-
-        if letra == palavra[6]:
-            l7 = letra
-            palavra[6] = "@"
-            acerto += 1
-
-        if letra == palavra[7]:
-            l8 = letra
-            palavra[7] = "@"
-            acerto += 1
-
-        if letra == palavra[8]:
-            l9 = letra
-            palavra[8] = "@"
-            acerto += 1
+        for i in range(len(palavra)):
+            if letra == palavra[i]:
+                acerto += 1
+                palavra[0] = "@"
+                l[i] = letra
     
     else:
         erro += 1
@@ -127,7 +88,7 @@ print(corpo)
 print(pernas)
 print(base1)
 print(base2)
-print(" ",l1,l2,l3,l4,l5,l6,l7,l8,l9)
+print(" ",l)
 print(palpites)
 
 if acerto >= 9:
